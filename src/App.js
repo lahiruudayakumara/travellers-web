41daaa8eab9eaaeb382f4b617536626d8c00e2a3
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-01-01 23:12:15 +0530

    remove useSelector

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import { useSelector } from 'react-redux';
-import { selectIsAuthenticated, selectUser } from './stores/slices/userSlice';
 import MainRouter from './routers/MainRouter';
 
 function App() {
-  const user = useSelector(selectUser);
-  const isAuthenticated = useSelector(selectIsAuthenticated);
-
-
   return (
     <>
       <MainRouter />
