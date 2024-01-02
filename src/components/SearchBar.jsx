2533e65540c49717b9b2ba15b28262905dbd7f86
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-01-02 23:24:07 +0530

    improve

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,33 @@
-import { Search } from '@mui/icons-material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SearchBar = () => {
+    const navigate = useNavigate();
+    const [query, setQuery] = useState('');
+    console.log(query);
+
+    const searchHandle = () => {
+      navigate(`/search/${query}`)
+    }
+
   return (
     <div className='pt-14'>
-      <div className='bg-white shadow-md flex justify-between h-14 rounded-md'>
-        <div className='flex items-center px-5 text-gray'>
+      <div className='bg-white shadow-md flex h-14 rounded-md'>
+        <div className='flex fle items-center px-5 text-gray'>
             <LocationOnIcon />
-            <input className='outline-none' type='text' placeholder='Destination' />
+            <input 
+                className='outline-none w-full' 
+                type='text' 
+                placeholder='Destination' 
+                onChange={(e) => setQuery(e.target.value)}
+            />
         </div>
-        <div className='bg-blue w-20 rounded-r-md flex text-white justify-center items-center'>
+        <div 
+            className='bg-blue w-20 cursor-pointer rounded-r-md flex text-white ml-auto justify-center items-center'
+            onClick={searchHandle}
+        >
             <SearchIcon />
         </div>
       </div>
