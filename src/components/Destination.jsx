2a419a81ed7d7e41abe793302e9492d1d4c7244f
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-01-02 23:23:28 +0530

    ui improve

---
+++ src/components/Destination.jsx
@@ -0,0 +1,65 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Grid, Mousewheel, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
+import DestinationPreview from './DestinationPreview';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase/Firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import { useDispatch } from 'react-redux';
+
+const Destination = () => {
+    const dispatch = useDispatch();
+    const [destination, setDestination] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const querySnapshot = await getDocs(collection(db, "destination")); 
+            console.log("DB Read...");
+      
+            const fetchedReviews = querySnapshot.docs.map((doc) => ({ ...doc.data() })); 
+            setDestination(fetchedReviews);
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+        };
+      
+        fetchData();
+      }, []);
+
+    const slidesPerView = window.innerWidth < 768 ? 1 : 3;
+
+
+
+  return (
+    <div id='destination' className='px-8 md:px-40 h-auto'>   
+        <h1 className='text-2xl text-blue font-bold'>Top Destinations</h1>
+        <div className='flex justify-between items-center mt-8'>
+        <Swiper
+            slidesPerView={slidesPerView}
+            grid={{
+            rows: 1,
+            }}
+            spaceBetween={50}
+            pagination={{
+            clickable: true,
+            }}
+            mousewheel={true}
+            modules={[Grid, Pagination, Mousewheel]}
+            className="mySwiper"
+        >
+          {destination.map((Obj, index)=>
+                <SwiperSlide key={index}>
+                    <DestinationPreview destination={Obj}  />
+                </SwiperSlide>
+          )}
+        </Swiper>             
+        </div>
+    </div>
+  )
+}
+
+export default Destination
--- src/components/DestinationPreview.jsx
+++ src/components/DestinationPreview.jsx
@@ -1,29 +1,35 @@
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
-import BookmarkOutlinedIcon from '@mui/icons-material/BookmarkOutlined';
 import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
-import StarOutlineOutlinedIcon from '@mui/icons-material/StarOutlineOutlined';
 import { useDispatch } from 'react-redux';
+import { addBookmarks } from '../stores/slices/bookmarksSlice';
 
-const DestinationPreview = ({name, img, country, rating}) => {
+const DestinationPreview = ({destination, icon}) => {
     const dispatch = useDispatch();
+    
+    const addBookMarks = () => {
+        dispatch(addBookmarks(destination));
+    }
     
   return (
     <div className='bg-white shadow-md rounded-md mb-10'>
         <img 
-            src={img}
+            src={destination.img}
             alt='place-holder'
             className='rounded-t-md md:h-44 md:w-full'
         />
         <div className='px-5 py-2'>
             <div className='flex justify-between items-center'>
-                <h3 className='text-lg text-blue font-bold'>{name}</h3>
-                <p className='text-yellow'>{rating}<StarOutlinedIcon  /></p>
+                <h3 className='text-lg text-blue font-bold'>{destination.name}</h3>
+                <p className='text-yellow'>{destination.rating}<StarOutlinedIcon  /></p>
             </div>
             <div className='flex justify-between items-center text-blue'>
-                <p className='text-brown font-semibold'>{country}</p>
-                <div className='hover:shadow-md hover:rounded-full cursor-pointer p-1'>
+                <p className='text-brown font-semibold'>{destination.country}</p>
+                {icon == true? null :
+                <div
+                    onClick={addBookMarks} 
+                    className='hover:shadow-md hover:rounded-full cursor-pointer p-1'>
                     <BookmarkBorderOutlinedIcon />
-                </div>
+                </div> }
             </div>
         </div>
     </div>
